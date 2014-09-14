95ce154a64dd6b5097c73539a222ba4cd75d7a2b
zhangjiayi-bjhl <rayanddoctor@126.com>
2014-09-14 09:47:36 +0800

    update

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,23 +1,31 @@
 module.exports = function(grunt) {
 
-  // Project configuration.
-  grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
-    uglify: {
-      options: {
-        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
-      },
-      build: {
-        src: 'src/<%= pkg.name %>.js',
-        dest: 'build/<%= pkg.name %>.min.js'
-      }
-    }
-  });
+    // Project configuration.
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        concat: {
+            // 这里是concat任务的配置信息。
+        },
+        uglify: {
+            // 这里是uglify任务的配置信息
+            options: {
+                banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+            },
+            build: {
+                src: 'src/<%= pkg.name %>.js',
+                dest: 'build/<%= pkg.name %>.min.js'
+            }
+        }
+    });
 
-  // Load the plugin that provides the "uglify" task.
-  grunt.loadNpmTasks('grunt-contrib-uglify');
+    // 任意数据。
+    my_property: 'whatever',
+    my_src_files: ['foo/*.js', 'bar/*.js'],
 
-  // Default task(s).
-  grunt.registerTask('default', ['uglify']);
+    // Load the plugin that provides the "uglify" task.
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+
+    // Default task(s).
+    grunt.registerTask('default', ['uglify']);
 
 };
