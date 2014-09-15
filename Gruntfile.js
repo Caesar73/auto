be17483fbd70c024386b5ff811c0c818c3ed39b6
zhangjiayi-bjhl <rayanddoctor@126.com>
2014-09-15 10:59:59 +0800

    update

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,19 +2,54 @@ module.exports = function(grunt) {
 
     // Project configuration.
     grunt.initConfig({
+        // refer to the values of properties
         pkg: grunt.file.readJSON('package.json'),
+
         concat: {
             // 这里是concat任务的配置信息。
             options: {
-              // 这里是任务级的Options，覆盖默认值
+                // 这里是任务级的Options，覆盖默认值
+
+                // define a string to put between each file in the concatenated output
+                separator: ';'
+            },
+            dist: {
+                // the files to concatenate
+                src: ['src/**/*.js'],
+
+                // the location of the resulting JS file
+                dest: 'dist/<%= pkg.name %>.js'
             },
             foo: {
-              // concat task "foo" target options and files go here.
+                // concat task "foo" target options and files go here.
+                files: [
+                    {
+                        src: ['src/aa.js', 'src/aaa.js'],
+                        dest: 'dest/a.js'
+                    },
+                    {
+                        src: ['src/aa1.js', 'src/aaa1.js'],
+                        dest: 'dest/a1.js'
+                    },
+                ],
             },
             bar: {
-              // concat task "bar" target options and files go here.
+                // concat task "bar" target options and files go here.
+                files: [
+                    {
+                        src: ['src/bb.js', 'src/bbb.js'],
+                        dest: 'dest/b/',
+                        nonull: true
+                    },
+                    {
+                        src: ['src/bb1.js', 'src/bbb1.js'],
+                        dest: 'dest/b1/',
+                        filter: 'isFile'
+                    },
+                ],
             },
         },
+
         uglify: {
             // 这里是uglify任务的配置信息
             bar: {
