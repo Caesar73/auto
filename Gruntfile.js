8a2e3448c47b5635ac7f5a71410bed8f52d6439b
zhangjiayi-bjhl <rayanddoctor@126.com>
2014-09-16 10:36:43 +0800

    update

--- Gruntfile.js
+++ Gruntfile.js
@@ -104,7 +104,7 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-contrib-qunit');
     grunt.loadNpmTasks('grunt-contrib-watch');
-    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-jscss');
 
     // Default task(s).
     // 在命令行上输入"grunt test"，test task就会被执行。
